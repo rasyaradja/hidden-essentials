ad834e169beb93d75eac2facd7862ac3ea95a386
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2024-07-23 02:02:52 +0700

    commit kedua: menambahkan file css, img, js dan mengubah index.html

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const navigation = document.querySelector("#navigation");
+
+document.addEventListener("scroll", () => {
+  navigation.classList.toggle("scroll", window.scrollY > 0);
+});
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        hijau: "#a6e22e",
+      },
+    },
+  },
+  daisyui: {
+    themes: ["dark"],
+  },
+};
