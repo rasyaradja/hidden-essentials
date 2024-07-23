702d2d1cf8dff3d35047e20c403932feb4409551
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2024-07-23 13:17:15 +0700

    commit 4: menambahkan audio dan mengubah tampilan agar lebih responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,28 @@ document.addEventListener("scroll", () => {
   navigation.classList.toggle("scroll", window.scrollY > 0);
 });
 
+const audioElement = document.getElementById("background-audio");
+const volumeToggle = document.getElementById("volume-toggle");
+
+function toggleVolume() {
+  if (volumeToggle.checked) {
+    audioElement.play();
+    localStorage.setItem("isAudioPlaying", "true");
+  } else {
+    audioElement.pause();
+    localStorage.setItem("isAudioPlaying", "false");
+  }
+}
+
+function loadAudioState() {
+  // Always set the default state to off
+  localStorage.setItem("isAudioPlaying", "false");
+  volumeToggle.checked = false;
+  audioElement.pause();
+}
+
+document.addEventListener("DOMContentLoaded", loadAudioState);
+
 tailwind.config = {
   theme: {
     extend: {
